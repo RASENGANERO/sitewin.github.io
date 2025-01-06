45541449e1ab578efb1cf3f8d9803cb22d3b2b09
RASENGANERO <www.223@list.ru>
2025-01-06 18:09:48 +0300

    Update 2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded",()=>{
     //let tg = window.Telegram.WebApp;
     //tg.sendData("some string that we need to send"); 
-    const data = { key: 'value' }; // Ваши данные
+    const data = {
+        userId: 123,
+        action: "register",
+        additionalInfo: "Some info"
+    };
     window.Telegram.WebApp.sendData(JSON.stringify(data));
 });
