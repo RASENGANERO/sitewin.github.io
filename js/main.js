b7bc9dfcbb90e2e2beb3c16f864daf08757b1ecd
RASENGANERO <www.223@list.ru>
2025-01-08 05:46:04 +0300

    Final site

--- js/main.js
+++ js/main.js
@@ -1,10 +1,66 @@
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]]; // Меняем местами
+    }
+    return array;
+}
+
+function playFunc(){
+    let datSign = document.getElementsByClassName('cell-img');
+    for(let i=0; i<datSign.length; i++){
+        datSign[i].setAttribute('src','img/block.svg');
+    }
+    let elementsArray = shuffle(Array.from(datSign));
+    let check = Number(document.getElementById('trapsAmount').textContent.toString());
+    let checkes = 0;
+    switch(check){
+        case 1:
+            checkes = 10;
+            break;
+        case 3:
+            checkes = 5;
+            break;
+        case 5:
+            checkes = 4;
+            break;
+        case 7:
+            checkes = 3;
+            break;
+    }
+    let numberOfElementsToSelect = checkes;
+    let uniqueElements = elementsArray.slice(0, numberOfElementsToSelect);
+    for (let i = 0; i < uniqueElements.length; i++) {
+        setTimeout(() => {
+            setTimeout(() => {
+                uniqueElements[i].setAttribute('src', 'img/star.svg');
+            }, 800);
+        }, i * 800);
+    }
+}
+function leftamont(){
+    let dt = Number(document.getElementById('trapsAmount').textContent.toString());
+    dt-=2;
+    if (dt<0) {
+        dt=1;
+    }
+    document.getElementById('trapsAmount').textContent=dt.toString();
+}
+function rightamont(){
+    let dt = Number(document.getElementById('trapsAmount').textContent.toString());
+    dt+=2;
+    if (dt>7) {
+        dt=7;
+    }
+    document.getElementById('trapsAmount').textContent=dt.toString();
+}
 document.addEventListener("DOMContentLoaded",()=>{
-    //let tg = window.Telegram.WebApp;
-    //tg.sendData("some string that we need to send"); 
-    const data = {
-        userId: 123,
-        action: "register",
-        additionalInfo: "Some info"
-    };
-    window.Telegram.WebApp.sendData(JSON.stringify(data));
+    if (document.getElementById('prev_preset_btn')!=null){
+        let leftbtn = document.getElementById('prev_preset_btn');
+        leftbtn.addEventListener('click',leftamont);
+        let rightbtn = document.getElementById('next_preset_btn');
+        rightbtn.addEventListener('click',rightamont);
+        let signBtn = document.getElementById('playButton');
+        signBtn.addEventListener('click',playFunc);
+    }
 });
